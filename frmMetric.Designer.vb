07b9cf8f70751214899cdc920bdaba65081c8d49
franc <franc@BLUE>
2024-05-16 21:56:25 -0700

    Add project files.

---
+++ frmMetric.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMetric
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMetric))
+        Me.lstVisitorTimes = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnAnalyze = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ClearnToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.lblAverageTime = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lstVisitorTimes
+        '
+        Me.lstVisitorTimes.FormattingEnabled = True
+        Me.lstVisitorTimes.Location = New System.Drawing.Point(272, 81)
+        Me.lstVisitorTimes.Name = "lstVisitorTimes"
+        Me.lstVisitorTimes.Size = New System.Drawing.Size(98, 160)
+        Me.lstVisitorTimes.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(46, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(333, 31)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Website Traffic Estimate"
+        '
+        'btnAnalyze
+        '
+        Me.btnAnalyze.BackColor = System.Drawing.Color.DarkKhaki
+        Me.btnAnalyze.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAnalyze.Location = New System.Drawing.Point(72, 81)
+        Me.btnAnalyze.Name = "btnAnalyze"
+        Me.btnAnalyze.Size = New System.Drawing.Size(170, 31)
+        Me.btnAnalyze.TabIndex = 2
+        Me.btnAnalyze.Text = "Enter Visitor Analytics"
+        Me.btnAnalyze.UseVisualStyleBackColor = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearnToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(434, 24)
+        Me.MenuStrip1.TabIndex = 4
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ClearnToolStripMenuItem
+        '
+        Me.ClearnToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClearToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.ClearnToolStripMenuItem.Name = "ClearnToolStripMenuItem"
+        Me.ClearnToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.ClearnToolStripMenuItem.Text = "Menu"
+        '
+        'ClearToolStripMenuItem
+        '
+        Me.ClearToolStripMenuItem.Name = "ClearToolStripMenuItem"
+        Me.ClearToolStripMenuItem.Size = New System.Drawing.Size(101, 22)
+        Me.ClearToolStripMenuItem.Text = "Clear"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 273)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(444, 152)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'lblAverageTime
+        '
+        Me.lblAverageTime.AutoEllipsis = True
+        Me.lblAverageTime.AutoSize = True
+        Me.lblAverageTime.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAverageTime.Location = New System.Drawing.Point(11, 242)
+        Me.lblAverageTime.Name = "lblAverageTime"
+        Me.lblAverageTime.Size = New System.Drawing.Size(0, 24)
+        Me.lblAverageTime.TabIndex = 6
+        '
+        'frmMetric
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SandyBrown
+        Me.ClientSize = New System.Drawing.Size(434, 422)
+        Me.Controls.Add(Me.lblAverageTime)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnAnalyze)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lstVisitorTimes)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "frmMetric"
+        Me.Text = "Website Traffic Metics"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnAnalyze As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ClearnToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ClearToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents lblAverageTime As Label
+    Friend WithEvents lstVisitorTimes As ListBox
+End Class
---
+++ frmMetric.vb
@@ -0,0 +1,84 @@
+﻿'' Author: Francisco Bumanglag
+'' Project: Website Traffic Metric 
+'' Assignment: Module 6 Exercise 
+'' Course: Adv Visual Basic, Santa Ana College
+'' Class: CMPR205 Dr. Kimberly Davis 
+'' Date: March 19, 2024
+
+
+Public Class frmMetric
+
+    Private Sub frmMetric_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnAnalyze_Click(sender As Object, e As EventArgs) Handles btnAnalyze.Click
+
+        ''declare variables for visitor time, average time, and total time.
+        Dim strVisitorTime As String
+        Dim intVisitorTime As Integer
+        Dim decAverageTime As Decimal
+        Dim intTotalTime As Integer = 0
+
+        ''declare variables.  messages include prompts for input, headings for input dialogs,
+        ''error messages for non-numeric or negative input, and a marker for a canceled input action
+        Dim strInputMessage As String = "Enter the number of seconds spent by visitor"
+        Dim strInputHeading As String = "Visitor Time"
+        Dim strNormalMessage As String = "Enter the number of seconds spent by visitor"
+        Dim strNonNumericError As String = "Error - Enter a numeric value for the time spent by visitor"
+        Dim strNegError As String = "Error - Enter a positive number for the time spent by visitor"
+        Dim strCancelClicked As String = ""
+
+        Dim intMaxNumberOfEntries As Integer = 12 ''maximum number of entries
+        Dim intNumberOfEntries As Integer = 1 ''counter for loop
+
+        'displays input box for visitor time with message count
+        strVisitorTime = InputBox(strInputMessage & intNumberOfEntries, strInputHeading, " ")
+
+        ''loops until max entries or cancel, validates numeric positive input, updates total time, and prompts again
+        Do Until intNumberOfEntries > intMaxNumberOfEntries Or strVisitorTime = strCancelClicked
+            If IsNumeric(strVisitorTime) Then
+                intVisitorTime = Convert.ToInt32(strVisitorTime)
+                If intVisitorTime > 0 Then
+                    lstVisitorTimes.Items.Add(intVisitorTime)
+                    intTotalTime += intVisitorTime ''accumulate total time
+                    intNumberOfEntries += 1
+                    strInputMessage = strNormalMessage
+                Else
+                    strInputMessage = strNegError
+                End If
+            Else
+                strInputMessage = strNonNumericError
+            End If
+            If intNumberOfEntries <= intMaxNumberOfEntries Then
+                strVisitorTime = InputBox(strInputMessage & intNumberOfEntries, strInputHeading, " ")
+            End If
+        Loop
+
+        ''calculates, displays average time if entries exist, else alerts
+        If intNumberOfEntries > 1 Then
+            lblAverageTime.Visible = True
+            decAverageTime = intTotalTime / (intNumberOfEntries - 1)
+            lblAverageTime.Text = "Average time spent: " & decAverageTime.ToString("F2") & " seconds"
+        Else
+            MsgBox("No time value was entered.", MsgBoxStyle.Information, "Input Needed")
+        End If
+    End Sub
+
+    Private Sub ClearToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearToolStripMenuItem.Click
+
+        ''clears list, hides average label, enables Analyze button
+        lstVisitorTimes.Items.Clear()
+        lblAverageTime.Visible = False
+        btnAnalyze.Enabled = True
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+
+        ''exit application
+        Close()
+
+    End Sub
+
+End Class
